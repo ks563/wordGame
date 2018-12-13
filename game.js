6fa9ea3927393f6551a153ba6801ae40dd457a3d
ks563 <karen.shea90@gmail.com>
2018-12-13 17:26:11 -0600

    tutoring changes

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 var wordSelection = ["sablet", "canele", "eclair", "financier", "dacquoise", "spritz", "macaron", "mendiant", "souffle", "beignet"];
 var wordIndex = 0;
-var maxTries = 8; // how many guesses the player has to guess the word
+var maxTries = 12; // how many guesses the player has to guess the word
 var guessedLetters = []; // the letters the player has guessed
 var wordAttempt = []; // where the guessed word will be built
 var remainingGuesses = 0; //how many guesses remain - will use an iterater
 var playerGuess = "";
-var wordArray = [];
+var wordArray;
+var currentWord = null;
+var matchedLetters = [];
 var wins = 0;
 var gameStarted = false;
 var gameFinished = false;
@@ -28,8 +30,8 @@ function resetGame() {
 
     //selects new word from wordSelection
     wordIndex = Math.floor(Math.random() * (wordSelection.length));
-    
-    console.log(wordIndex);
+
+    // console.log(wordIndex);
 
     //empties arrays and previous guesses attempts
     guessedLetters = [];
@@ -43,37 +45,68 @@ function resetGame() {
     updateDisplay();
 };
 
-function checkLetter(){
-    wordArray = [wordSelection[wordIndex]];
-    wordArray.toString().split(" ");
-    for (var j =0; j < wordArray.length; j++){
-
-    if (playerGuess === wordArray[j]) {
-        wordArray.splice(j, 1, playerGuess); }
-    else; 
-        guessedLetters.push(playerGuess);
-    }
+function setUpGame(); {
+    currentWord = wordSelection[wordIndex];
+    wordArray = currentWord.toString().split("");
+    // update wordAttempt
+    // update guesses
 };
 
-function guessCounter() {
-    for (var k; k < maxTries; k++){
-        //when playerGuess occurs, -- from remainingGuesses
+function updateDisplay() {
+    if (remainingGuesses === 0) {
+        resetGame();
+        checkMatchedLetter();
+        checkIncorrectLetter();
+    };
+
+    // update wordAttempt
+    // check if user won
+};
+
+function checkMatchedLetter(playerGuess) {
+    for (var j = 0; j < wordArray.length; j++) {
+
+        if (playerGuess === wordArray[j] && matchedLetters.indexOf(playerGuess) === -1) {
+            wordArray.splice(j, 1, playerGuess);
+            matchedLetters.push(playerGuess);
+        };
+    };
+}
+
+function checkIncorrectLetter(playerGuess) {
+    if (wordArray.indexOf(playerGuess) === -1 && guessedLetters.indexOf(playerGuess) === -1) {
+        guessedLetters.push(playerGuess);
+    };
+};
+
+function updateWordAttempt() {
+    var wordDisplay = "":
+    for (var j = 0; j < wordArray.length; j++){
+
     }
+
+}
+
+
+function guessCounter() {
+    for (var k; k < maxTries; k++) {
+        //when playerGuess occurs, -- from remainingGuesses
+    };
 };
 
 resetGame();
- // initalizes game if there are remaining guesses and starts the game
+// initalizes game if there are remaining guesses and starts the game
+
 function startGame(event) {
-    if (remainingGuesses > 0);
-    if (!gameStarted){
+
+    if (!gameStarted) {
         gameStarted = true;
     };
 
 };
 
 document.onkeypress = function (event) {
-        playerGuess = event.key;
-        startGame();
-        checkLetter();
-    };
-
+    playerGuess = event.key;
+    startGame();
+    checkLetter();
+};
